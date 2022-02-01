f5dd28f925e91f57be66896f9cc7598ed7bcddaa
yoavshapira1 <yoavshapira1@gmail.com>
2022-02-01 19:26:30 +0200

    density of group added

--- Main.py
+++ Main.py
@@ -6,11 +6,8 @@ from pylsl import StreamInfo, StreamOutlet
 from pythonosc.udp_client import SimpleUDPClient
 import numpy as np
 
-# TODO: read this website page
-# https://kivy.org/doc/stable/guide/inputs.html#touch-events
-# and look for more touch possibilities
-# TODO: check if the velocity and time_tocuh been updated when just STAYING.
 # TODO: consider using built-in attribute of touch events
+# TODO: complete the group density attribute
 
 # UDP details
 IP = "127.0.0.1"
@@ -43,8 +40,8 @@ ORIGIN_dict = {
             }
 
 # The actual parameters fed into the machine
-ORIGIN = "Center"
-GRID = "Circular"
+ORIGIN = "Bottom_left"
+GRID = "Rectangle"
 
 # don't change this:
 parameters = [ORIGIN, GRID]
@@ -72,40 +69,61 @@ class TouchEvent:
         self.grid = GRID_dict[grid]
         self.origin_string = origin
         self.grid_string = grid
+        self.group = []
 
         # data-generating related
         self.dt = 0.01                      # The delta in time which used to calculate velocity
         self.touch_time = 0
         self.max_norm = np.linalg.norm(np.array([1, 1]) - self.origin)
-        self.reduce_time_threshold = self.max_norm / 50  # ||pos - prev_pos|| > threshold => reduce time_touch
+        self.reduce_time_threshold = self.max_norm / 300  # ||pos - prev_pos|| > threshold => reduce time_touch
         self.vel_normalization = 5          # An arbitrary number - was chosen by observations
         self.start_pos = np.array([0, 0])
+        self.mode = 0
         # initialize the values to zero
         self.deactivate()
 
     def activate(self):
+        """When a touch event is begin"""
         self.prev_pos = self.cur_pos
         self.start_pos = self.cur_pos
+        self.time_start = time.time()
         self.switch = True
 
     def deactivate(self):
+        """After a touch event is leaving the screen"""
         self.cur_pos = np.array([0, 0])
         self.prev_pos = np.array([0, 0])    # The previous position is been updated every dt
         self.prev_pos_time = time.time()
+        if self.touch_time < 0.03:
+            self.next_mode()
         self.touch_time = 0
         self.switch = False
 
+    def next_mode(self):
+        """For a very short touch"""
+        self.mode += 1
+
+    def prev_mode(self):
+        """For a very short double-touch"""
+        self.mode = max(self.mode - 1, 0)
+
     def move(self, pos):
+        """In every movement, and only if move"""
         # change previous position only if dt time has passed
         if time.time() - self.prev_pos_time > self.dt:
             # reduce the time touch value if position is changed a lot
             ds = np.linalg.norm(self.cur_pos - self.prev_pos)
             if ds > self.reduce_time_threshold:
-                self.touch_time *= ds       # ds < 1 so results in reducing value
+                self.touch_time *= 0.1       # ds < 1 so results in reducing value
             self.prev_pos = self.cur_pos
             self.prev_pos_time = time.time()
             self.touch_time += self.dt
         self.cur_pos = np.array(pos)
+
+    def add_to_group(self, touch):
+        """Add a touch event to the group of this one.
+        NOTICE: The touch event is a Kivy based! follows "TouchInput" API"""
+        self.group.append(touch)
 
     def rename_id(self, id):
         self.id = id
@@ -122,15 +140,27 @@ class TouchEvent:
     def get_prev_pos(self):
         return self.prev_pos
 
-    def get_data(self):
+    def get_start_time(self):
+        return self.time_start
+
+    def get_qualitiative_data(self):
+        """Generate the data sent to UDP. The data is a list of concatenated values:
+        [start_pos_x, start_pos_y, pos_x, pos_y, velocity, touch_time, mode, density]"""
         # if this channel is note active
         if not self.switch:
-            return self.start_pos.tolist() + [0.0, 0.0, 0.0, 0.0]
+            return [0.0] * 8
 
-        # ELSE: generate proper data set
-        return self.generate_data()
+        position = self.get_positional_data()
+        start_pos = [self.start_pos[0], self.start_pos[1]]
+        velocity = [min(1.0, self.get_velocity())]
+        touch_time = [self.get_touch_time()]
+        mode = [self.mode]
+        density = [self.get_density()]
+        return density
+        # return start_pos + position + velocity + touch_time + mode + density
 
-    def circular_rep(self):
+    def positional_circular_rep(self) -> list:
+        """Generate the circular positional attributes"""
         # 'raw' euclidean distance of the position from the origin
         dist = self.cur_pos - self.origin
         norm = np.linalg.norm(dist)
@@ -143,8 +173,9 @@ class TouchEvent:
         return [radius, tan]
 
     def get_positional_data(self) -> list :
+        """Generate the positional attributes"""
         if self.grid_string == "Circular":
-            return self.circular_rep()
+            return self.positional_circular_rep()
 
         # else => Origin maybe not CENTER
         # If origin is center, the normalization is just *2 for both axis
@@ -160,6 +191,7 @@ class TouchEvent:
             return self.cur_pos.tolist()
 
     def get_velocity(self):
+        """Calculate velocity in respect to time interval of self.dt"""
         # calculate pure velocity
         vel = np.linalg.norm(self.cur_pos - self.prev_pos) / self.dt
         # normalize: dividing by arbitrary number that was chosen by observations
@@ -169,22 +201,23 @@ class TouchEvent:
         # sigmoid function
         if self.touch_time < 0.07:
             return 1 / (1 + np.exp((-20*x) +4))
-        return 1 / (1 + np.exp((-x/2) + 4))
+        return 1 / (1 + np.exp(-x + 4))
 
     def get_touch_time(self):
+        """Calculate the continuously touching time"""
         return self.touch_time_function(self.touch_time)
 
-    def generate_data(self) -> list:
-        """
-        Generate the data sent to UDP. The data is a list of concatenated values:
-        [pos_x, pos_y, vel_, touch_time]
-        """
-        position = self.get_positional_data()
-        start_pos = [self.start_pos[0], self.start_pos[1]]
-        velocity = [min(1.0, self.get_velocity())]
-        touch_time = [self.get_touch_time()]
-        return start_pos + position + velocity + touch_time
-
+    def get_density(self):
+        """Calculate the density of the touch's group.
+        If the group is empty, this value equals to 0"""
+        if self.group is not None:
+            touches = np.empty(shape=(len(self.group)+1,2))
+            for t in range(len(self.group)):
+                touches[t] = [self.group[t].sx, self.group[t].sy]
+            touches[-1] = self.cur_pos
+            return np.linalg.norm(touches)
+        else:
+            return 0.0
 
     def __repr__(self):
         return "Id :{}, Active?: {},Position: {}".format(self.id, self.switch, self.cur_pos)
@@ -266,7 +299,7 @@ class DataBroadcaster:
         positional_data = []
         for ch in self.channels:
             positional_data += ch.get_pos_as_list()
-            generated_data += ch.get_data()
+            generated_data += ch.get_qualitiative_data()
 
         # Broadcasting the positional_data (i.e to LSL)
         for client in self.positional_clients:
@@ -287,14 +320,25 @@ class TouchInput(Widget):
     def on_touch_down(self, touch):
         # If not touch mode type, do nothing
         if touch.device == self.touch_mode:
-
+            last_ch = None
             # Check if any available channel
             for ch in [*self.channels, self.waiting_ch]:
                 if not ch.isActive():
-                    ch.move([touch.sx, touch.sy])
-                    ch.rename_id(touch.id)
-                    ch.activate()
+                    # if delta time from the previous touch is small, add to group
+                    if last_ch:
+                        print("there is last channel")
+                        if time.time() - last_ch.get_start_time() < 0.3:
+                            print("time elpased :", time.time() - last_ch.get_start_time())
+                            last_ch.add_to_group(touch)
+                            break
+                    if touch.is_double_tap:
+                        ch.prev_mode()
+                    else:
+                        ch.move([touch.sx, touch.sy])
+                        ch.rename_id(touch.id)
+                        ch.activate()
                     break
+                last_ch = ch
 
     def on_touch_move(self, touch):
         if touch.device == self.touch_mode:
