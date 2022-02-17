2ccd75f25b07f3ad76423b52b697d0717472bb73
Yoavshapira1 <62549504+Yoavshapira1@users.noreply.github.com>
2022-02-17 10:34:52 +0200

    Set: Full window with no limits

---
+++ KivyTrials.py
@@ -0,0 +1,29 @@
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.uix.label import Label
+
+
+class Base(Label):
+    def __init__(self, **kwargs):
+        super(Base, self).__init__(**kwargs)
+        Window.bind(on_request_close=self.exit_check, )
+        self.counter = 0
+        self.text = str(self.counter)
+
+    def exit_check(self, *args):
+        self.counter += 1
+        if self.counter < 5:
+            self.text = str(self.counter)
+            return True  # block app's exit
+        else:
+            return False  # let the app close
+
+
+class SampleApp(App):
+    def build(self):
+        return Base()
+
+
+if __name__ == "__main__":
+
+    SampleApp().run()
