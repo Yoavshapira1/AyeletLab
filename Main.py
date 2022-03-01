c5bc94b6310d1d28d78ff92e4eeee2999cf1ae2f
yoavshapira1 <yoavshapira1@gmail.com>
2022-03-01 16:56:12 +0200

    area fixed for TWO componenets only

--- Main.py
+++ Main.py
@@ -401,9 +401,9 @@ if __name__ == "__main__":
 
     # Avoiding the user from accidentally close the app
     # App closes ONLY if <escape> is pressed
-    # Window.fullscreen = True
-    # Window.borderless = True
-    # Window.maximize()
+    Window.fullscreen = True
+    Window.borderless = True
+    Window.maximize()
     Window.exit_on_escape = True
 
     # Run the app
