6bdb14bfeb87adb46340d9456d3d1cd6e0a52a07
yoavshapira1 <yoavshapira1@gmail.com>
2022-04-14 12:12:31 +0300

    Start to analyze

--- Tapper/Analyzer.py
+++ Tapper/Analyzer.py
@@ -6,27 +6,39 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from os import getcwd
 
-TAIL = 30       # Tail of the animation factor
-SPEED = 0.2       # FastForWard factor
-FRAME_COUNTER = 0
+ANIMATION_TAIL = 30         # Tail of the animation factor
+ANIMATION_SPEED = 0.2       # FastForWard factor
+FRAME_COUNTER = 0           # don't change this
+
+TRIM_CONST = 2               # time of the beginning of the trial to be cut, in sec.
+
 
 def extract_data(path):
-    """Extract the data from a given file (as path)"""
+    """Extract the data from a given file (as path)
+    ":return ndarray contains the data. array shape changes due to the session:
+             For Free Motion/Circles sessions: (n_samples, 2) holds the (x,y) position of each sample
+             For Tapping:                      (n_samples, )  holds the timing of each sample
+     """
     arr = path.split('\\')
     session = arr[-1].split('_')[0]
     col_list = head_lines[session]
     data = pd.read_csv(path, usecols=col_list)
-    name, trial = data.iloc[0]['subject'].split('_')
-    samples_count = len(data.index)
+    name, trial = data.iloc[0]['subject'].split('_')[0], data.iloc[0]['subject'].split('_')[-1]
+    n_samples = len(data.index)
     time_length = data.iloc[-2]["tapNum"]
     if session in [FREE_MOTION, CIRCLES]:
         data = np.dstack((data['x_pos'].to_numpy(), data['y_pos'].to_numpy()))[0]
     else:
         data = data['natRhythmTap (in ms.)'].to_numpy()
-    return data, name, trial, session, samples_count, time_length
+    start_trim = int(TRIM_CONST / time_length * n_samples)
+    return data[start_trim:], name, trial, session, n_samples - start_trim, time_length - TRIM_CONST
 
-def animate(data, name, trial, session, samples_count, time_length):
+
+def animate_free_movement(data, name, trial, session, n_samples, time_length):
     """Animate the coordinates of a single session"""
+
+    if session not in [FREE_MOTION, CIRCLES]:
+        raise Exception("Function 'animate_free_movement' can only work with data from sessions 'FREE_MOTION' or 'CIRCLES'")
 
     fig, ax = plt.subplots(figsize=(16,9))
     ax.set_title("subject: %s, %s session number %d" % (name, session, int(trial)+1), fontdict=None, loc='center', pad=None)
@@ -45,17 +57,32 @@ def animate(data, name, trial, session, samples_count, time_length):
     def update(frame):
         global FRAME_COUNTER
         FRAME_COUNTER -= 1
-        if session in [CIRCLES, FREE_MOTION]:
-            if FRAME_COUNTER == 0:
-                plt.pause(2)
-                plt.close(fig)
-            xdata.append(frame[0])
-            ydata.append(frame[1])
-            ln.set_data(xdata[-TAIL:], ydata[-TAIL:])
+        if FRAME_COUNTER == 0:
+            plt.pause(1)
+            plt.close(fig)
+        xdata.append(frame[0])
+        ydata.append(frame[1])
+        ln.set_data(xdata[-ANIMATION_TAIL:], ydata[-ANIMATION_TAIL:])
         return ln,
 
     ani = FuncAnimation(fig, update, frames=data, init_func=init,
-                        interval=time_length / samples_count / SPEED, blit=True, repeat=False)
+                        interval=time_length / n_samples / ANIMATION_SPEED, blit=True, repeat=False)
+    plt.show()
+
+
+def get_velocity_vector(data, session):
+    if session not in [FREE_MOTION, CIRCLES]:
+        raise Exception("Function 'get_velocity_vector' can only work with data from sessions 'FREE_MOTION' or 'CIRCLES'")
+
+    dt = time_length / n_samples
+    dist = np.linalg.norm(data[1:] - data[:-1], axis=1)
+    return dist / dt
+
+
+def plot(data, title, ytitle, xtitle):
+    plt.clf()
+    plt.title(title)
+    plt.plot(data)
     plt.show()
 
 
@@ -65,8 +92,11 @@ if __name__ == "__main__":
     # MAKE SURE you copy the full path of the file, including the hardrive prefix.
     # ALSO make sure to maintain the format: r"PATH"
     # For the example file (Tapper > Data > Example_0 > Circles_1.csv);
-    path = r"C:\Users\yoavsha\Desktop\LSL\Tapper\Data\Example_0\Circles_1.csv"
+    path = r"C:\Users\yoavsha\Desktop\LSL\Tapper\Data\Const_Vel_0\Motion_line_1.csv"
 
-    data, name, trial, session, samples_count, time_length = extract_data(path)
-    animate(data, name, trial, session, samples_count, time_length)
+    data, name, trial, session, n_samples, time_length = extract_data(path)
+    animate_free_movement(data, name, trial, session, n_samples, time_length)
+    plot(get_velocity_vector(data, session), title="Velocity in time", xtitle="Velocity", ytitle="Samples")
+
+
 
